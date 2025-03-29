649c222b12cdf8ef226d0c2e0b3b4ee3b0e4c388
Sergio González Martínez <1018sgm1018@gmail.com>
2025-03-29 14:47:31 +0100

    Feat: added ADMIN role guard bypass

--- api/src/auth/guards/roles.guard.ts
+++ api/src/auth/guards/roles.guard.ts
@@ -2,6 +2,7 @@ import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { Observable } from 'rxjs';
 import { User } from 'src/users/entities/user.entity';
+import { ROLES_KEY } from '../decorators/roles.decorator';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
@@ -10,8 +11,11 @@ export class RolesGuard implements CanActivate {
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    const roles = this.reflector.get<string[]>('roles', context.getHandler());
+    const roles = this.reflector.get<string[]>(ROLES_KEY, context.getHandler());
     if (!roles) {
+      return true;
+    }
+    if (this.matchRoles(roles, ['admin'])) {
       return true;
     }
     const request = context.switchToHttp().getRequest();
