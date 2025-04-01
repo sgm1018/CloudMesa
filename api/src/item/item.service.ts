b87fd76ae13514e348a3325c690e9d78bdfa1828
Sergio González Martínez <1018sgm1018@gmail.com>
2025-04-01 20:43:33 +0200

    Feat: find items services by userid

--- api/src/item/item.service.ts
+++ api/src/item/item.service.ts
@@ -4,10 +4,25 @@ import { Model } from 'mongoose';
 import { Item } from './entities/item.entity';
 
 import { BaseService } from 'src/base/base.service';
+import { ApiResponse } from 'src/shared/responses/ApiResponse';
 
 @Injectable()
 export class ItemsService extends BaseService<Item> {
   constructor(@InjectModel(Item.name) private readonly ItemModel: Model<Item>) {
     super(ItemModel);
   }
+
+
+  async findItemByUser(userId : string, itemId : string) : Promise<ApiResponse<Item>>{
+    const item = await this.findOne({ _id: itemId, userId: userId });
+    if (!item.isSuccess()){
+      return ApiResponse.error(-1, `Item with ID "${itemId}" not found for user "${userId}"`);
+    }
+    return ApiResponse.item(item.value!);
+  } 
+
+  async findItemwByUserByParentIdPagination (userId : string, parentId : string, page: number, limit: number) : Promise<ApiResponse<Item[]>>{
+
+  }
+
 }
