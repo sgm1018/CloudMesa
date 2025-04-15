4444429f5f49e57b8dc1664d753652cee8b04457
sgm1018 <1018sgm1018@gmail.com>
2025-04-15 23:28:18 +0200

    Fix: fix loginDto

--- api/src/auth/dto/loginToken.dto.ts
+++ api/src/auth/dto/loginToken.dto.ts
@@ -2,7 +2,7 @@ import { RefreshToken } from './../../users/entities/RefreshToken.entity';
 import { UserGetDto } from 'src/users/dto/user.dto';
 import { User } from './../../users/entities/user.entity';
 export class LoginTokenDto{
-    accesToken : string;
+    accessToken : string;
     refreshToken : string;
     user : UserGetDto;  
 
@@ -10,7 +10,7 @@ export class LoginTokenDto{
 
     createFromUser(User : User, AccesToken: string, RefreshToken : string) : LoginTokenDto{
         this.user = UserGetDto.fromUser(User);
-        this.accesToken = AccesToken;
+        this.accessToken = AccesToken;
         this.refreshToken = RefreshToken;
         return this;
     }
